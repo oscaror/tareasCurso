79abfb8ca3204f13543f054d3cf8ddab58d22741
Oscar Ortega <Ozz@macbook-pro-de-oscar-5.local>
2016-03-29 21:50:50 -0600

    commitF

--- juegoDeMemoria.playground/Contents.swift
+++ juegoDeMemoria.playground/Contents.swift
@@ -12,16 +12,16 @@ print ("Tarea correspondiente a la semana 2 \nRealizada por Oscar Ortega \n")
 
 for i in rango {
     
-    if i >= 30 && i <= 40{
+    if i >= 30 && i <= 40{ //rango del 30 al 40 msj: viva Swift!!
         print ("\(i) Viva Swift!!!")
     }
-    if i % 5 == 0 && i != 0 {
+    if i % 5 == 0 && i != 0 { //Divisible entre 5 y se verifica que la division /0 msj: "Bingo!!!"
         print("\(i) Bingo!!!")
     }
-    if i % 2 == 0{
+    if i % 2 == 0{ // Par msj: es par!
         print("\(i) es par")
     }
-    if i % 2 == 1{
+    if i % 2 == 1{ // impar msj: es impar
         print ("\(i) es impar")
     }
     
