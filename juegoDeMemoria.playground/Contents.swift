cc25b788ab4a632a2096c30355d800ac8563f6bb
Oscar Ortega <Ozz@macbook-pro-de-oscar-5.local>
2016-03-29 20:24:34 -0600

    commit4

---
+++ juegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var rango = 0...100
+
+for i in rango {
+    
+    if i >= 30 && i <= 40{
+        print ("\(i) Viva Swift!!!")
+    }
+    if i % 5 == 0 && i != 0 {
+        print("\(i) Bingo!!!")
+    }
+    if i % 2 == 0{
+        print("\(i) es par")
+    }
+    if i % 2 != 0{
+        print ("\(i) es impar")
+    }
+    
+    //fin de la tarea
+    
+}
